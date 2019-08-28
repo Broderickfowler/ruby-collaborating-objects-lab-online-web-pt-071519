2d21c4d42d9fafe2b3e2855344b41204e1a859e5
Broderick  Fowler Jr <broderickfowler@outlook.com>
2019-08-28 13:58:29 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,42 @@
+class Artist
+attr_accessor :name
+@@all = []
+
+def self.all
+  @@all
+end
+
+def initialize(name)
+  @name = name
+  @@all << self
+  end
+
+def songs
+  #song = Song.new(artist)
+  Song.all.select do |song|
+    song.artist == self
+  end
+end
+
+def add_song(song)
+  song.artist = self
+end
+
+def self.find_or_create_by_name(name)
+  if artist = self.find(name)
+    artist
+  else
+    self.new(name) #self in this case is the receiver of a new artist.
+  end
+end
+
+def self.find(name)
+  @@all.find{|artist| artist.name == name}
+end
+
+def print_songs
+  songs.each do |song|
+    puts #{song.name}
+    end
+  end
+end
