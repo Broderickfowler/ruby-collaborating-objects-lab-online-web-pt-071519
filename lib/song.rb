932da390e715e8cadfdddf08fdf57620552dab38
Broderick  Fowler Jr <broderickfowler@outlook.com>
2019-08-28 14:05:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,7 +12,11 @@ def self.all
   @@all
 end
 
-
+def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
+  
   def self.new_by_filename(file)
     song_info = file.chomp(".mp3").split("-")
     song = s0ong.new(song_info[1])
