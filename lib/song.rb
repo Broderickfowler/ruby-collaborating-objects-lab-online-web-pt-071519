5f9e89da09916e5ed70bcbf4330c7e31276ef6c7
Broderick  Fowler Jr <broderickfowler@outlook.com>
2019-08-28 14:08:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,7 +18,7 @@ def artist_name=(name)
   end
 
   def self.new_by_filename(file)
-    song_info = file.chomp(".mp3").split("-")
+    song_info = file.chomp(".mp3").split(" - ")
     song = Song.new(song_info[1])
     song.artist_name = song_info[0]
     song
